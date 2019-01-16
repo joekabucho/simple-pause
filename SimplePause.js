6b4401bda350fb0da87a4904a00d2d2933507b5a
joe kabucho <32999243+joekabucho@users.noreply.github.com>
2019-01-16 17:09:36 +0300

    Add files via upload

---
+++ SimplePause.js
@@ -0,0 +1,40 @@
+﻿#pragma strict
+
+private var pauseGame : boolean = false;
+private var showGUI : boolean = false;
+
+function Update()
+{
+	if(Input.GetKeyDown("p"))
+	{
+		pauseGame = !pauseGame;
+		
+    	if(pauseGame == true)
+    	{
+    		Time.timeScale = 0;
+    		pauseGame = true;
+    		GameObject.Find("Main Camera").GetComponent(MouseLook).enabled = false;
+    		GameObject.Find("First Person Controller").GetComponent(MouseLook).enabled = false;
+    		showGUI = true;
+    	}
+    }
+    
+    if(pauseGame == false)
+    {
+    	Time.timeScale = 1;
+    	pauseGame = false;
+    	GameObject.Find("Main Camera").GetComponent(MouseLook).enabled = true;
+    	GameObject.Find("First Person Controller").GetComponent(MouseLook).enabled = true;
+    	showGUI = false;
+    }
+    
+    if(showGUI == true)
+    {
+    	  gameObject.Find("PausedGUI").guiTexture.enabled = true;  
+    }
+    
+    else
+    {
+    	gameObject.Find("PausedGUI").guiTexture.enabled = false;  
+    }
+}
